6d247c94220d391ac1ac0c35a439a572ae497e33
Samuel Yakubu <samuelyakubu737@gmail.com>
2022-03-01 02:35:18 +0100

    set up

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,13 +8,13 @@ export const Nav = ()=> {
                 <NavLogo />
             </div>
             <ul className="nav__lists"> 
-                <li className="nav__lists--item"><a href='#'>Features</a></li>  
-                <li className="nav__lists--item"><a href='#'>Pricing</a></li>  
-                <li className="nav__lists--item"><a href='#'>Resources</a></li>  
+                <li className="nav__lists--item"><a href='/#' className="no-deco">Features</a></li>  
+                <li className="nav__lists--item"><a href='/#' className="no-deco">Pricing</a></li>  
+                <li className="nav__lists--item"><a href='/#' className="no-deco">Resources</a></li>  
             </ul>
             <div className="nav__cta">
-                <a href='#'>Login</a>
-                <a href='#'>Sign Up</a>
+                <a href='/#' className="no-deco">Login</a>
+                <a href='/#' className="no-deco">Sign Up</a>
             </div>
         </nav> 
     )
