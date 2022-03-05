862b1a8b278819ded89590691d753291d0658b3b
Samuel Yakubu <samuelyakubu737@gmail.com>
2022-03-05 15:58:33 +0100

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react"; 
 import { ReactComponent as Illustration } from "./illustration-working.svg";
+import IllustrationLogo from './illustration-working.svg'
 export const Hero = ()=> { 
     return ( 
         <section className="hero">
@@ -14,7 +15,7 @@ export const Hero = ()=> {
             </div>
         </div>
         <div className="hero__image">
-            <Illustration /> 
+            <img src={IllustrationLogo} /> 
         </div>
         </section>
     )
